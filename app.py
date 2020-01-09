7a9394aa8bc297bccaa74531fa68f079c56b8e28
Łukasz Kwasek <pamput@gmail.com>
2020-01-09 12:34:44 +0000

    add poster as an endpoint move token as a parameter

--- app.py
+++ app.py
@@ -1,30 +1,39 @@
 from flask import Flask
+from flask import request
 
 import burned_kanji.burned_kanji as burned
 import recent_fail.recent_fail as fails
 import verbs_export.verbs_to_html as verbs
+import kanji_poster.kanji_poster as poster
 
 app = Flask(__name__)
 
 
-@app.route("/<token>/fails")
-def do_fails(token):
+@app.route("/fails")
+def do_fails():
     return fails.get_recent_fails_html(
-        token
+        request.args.get('token', type=str)
     )
 
 
-@app.route("/<token>/verbs")
-def do_verbs(token):
+@app.route("/verbs")
+def do_verbs():
     return verbs.get_all_verbs(
-        token
+        request.args.get('token', type=str)
     )
 
 
-@app.route("/<token>/burns")
-def do_burns(token):
+@app.route("/burns")
+def do_burns():
     return burned.get_burned_kanji(
-        token
+        request.args.get('token', type=str)
+    )
+
+
+@app.route("/poster")
+def do_poster():
+    return poster.get_kanji_poster(
+        request.args.get('token', type=str)
     )
 
 
