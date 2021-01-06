9d75478671d30f94381bef29778ea51bc28c95c5
Lukasz Kwasek <pamput@gmail.com>
2021-01-06 19:54:06 +0000

    temporarily remove README.md from setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
     name="wanikani-tools",
     version="0.0.1",
     author="Lukasz Kwasek",
     author_email="lukasz@kwasek.co.uk",
     description="A set of small tools to improve your japanese learning",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
     url="https://github.com/pamput/wanikani-tools",
     packages=['wanikani-tools'],
     install_requires=[
